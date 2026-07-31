36665c6e3934d4dd8408177d37e89d23a2c6e454
Konrad Ciesielski <kondykox@gmail.com>
2026-07-31 13:25:40 +0200

    feat: buttons to start/stop shift

--- src/components/EditPanel.tsx
+++ src/components/EditPanel.tsx
@@ -1,5 +1,7 @@
-import type { Dispatch, SetStateAction } from "react";
+import { useRef, type Dispatch, type SetStateAction } from "react";
 import type { Profile } from "../types/Profile";
+import { saveProfile } from "../utils/saveProfile";
+import type { ActiveShift } from "../types/Shift";
 
 interface EditPanelProps {
   editedProfile: Profile;
@@ -7,14 +9,31 @@ interface EditPanelProps {
 }
 
 const EditPanel = ({ editedProfile, setEditedProfile }: EditPanelProps) => {
+  const clockInRef = useRef<HTMLInputElement>(null);
+
+  const handleStartShift = () => {
+    const newActiveShift: ActiveShift = {
+      clockIn: new Date().toISOString(),
+      rate: editedProfile.hourlyRate,
+    };
+
+    setEditedProfile((prev) =>
+      prev ? { ...prev, activeShift: newActiveShift } : prev,
+    );
+
+    clockInRef.current?.focus();
+  };
+
   return (
-    <div className="bg-green-400 rounded-lg px-3 py-4 border-2 border-green-400">
+    <div className="bg-green-400 rounded-lg px-4 py-6 shadow-sm shadow-green-400">
       <h3 className="text-center text-white font-bold mb-4 text-xl">
         Panel edycji
       </h3>
+
+      {/* Stawka godzinowa */}
       <div className="input-group">
         <label htmlFor="hourlyRate">Stawka Godzinowa</label>
-        <div className="relative">
+        <div>
           <input
             type="number"
             id="hourlyRate"
@@ -27,8 +46,44 @@ const EditPanel = ({ editedProfile, setEditedProfile }: EditPanelProps) => {
                 prev ? { ...prev, hourlyRate: Number(e.target.value) } : prev,
               )
             }
+            onBlur={() => saveProfile(editedProfile)}
           />
           <span>zł/h</span>
+        </div>
+      </div>
+
+      {/* Zmiana w pracy */}
+      <div className="input-group">
+        <label htmlFor="activeShift">Zmiana w pracy</label>
+        <div className="flex flex-col gap-4" id="activeShift">
+          <div className="flex flex-col gap-2">
+            <input
+              ref={clockInRef}
+              type="datetime-local"
+              id="clockIn"
+              name="clockIn"
+              value={editedProfile.activeShift?.clockIn ?? ""}
+              onChange={(e) =>
+                setEditedProfile((prev) =>
+                  prev && prev.activeShift
+                    ? {
+                        ...prev,
+                        activeShift: {
+                          ...prev.activeShift,
+                          clockIn: e.target.value,
+                        },
+                      }
+                    : prev,
+                )
+              }
+            />
+            {editedProfile.activeShift && (
+              <input type="datetime-local" id="clockOut" name="clockOut" />
+            )}
+          </div>
+          <button className="btn" onClick={() => handleStartShift()}>
+            {!editedProfile.activeShift ? "Start" : "Stop"}
+          </button>
         </div>
       </div>
     </div>
