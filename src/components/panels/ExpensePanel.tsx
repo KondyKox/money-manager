7c96ccb0b3de8378be1af4c80663a6d8d8e90f7c
Konrad Ciesielski <kondykox@gmail.com>
2026-08-18 08:09:05 +0200

    feat: filter expenses

--- src/components/panels/ExpensePanel.tsx
+++ src/components/panels/ExpensePanel.tsx
@@ -3,7 +3,10 @@ import AddExpenseModal from "../modal/addExpense-modal";
 import CollapsablePanel from "../ui/CollapsablePanel";
 import type { DashboardElement } from "../../types/Dashboard";
 import ExpenseElement from "../ui/ExpenseElement";
-import type { ExpenseCategory } from "../../constants/expenseCategories";
+import {
+  EXPENSE_CATEGORIES,
+  type ExpenseCategory,
+} from "../../constants/expenseCategories";
 
 const ExpensePanel = ({
   editedProfile,
@@ -16,6 +19,7 @@ const ExpensePanel = ({
   const [selectedCategory, setSelectedCategory] =
     useState<ExpenseCategory | null>(null);
 
+  // expenses that we use here
   const filteredExpenses = editedProfile.expenses.filter((expense) => {
     const matchesMonth = expense.date.startsWith(selectedMonth);
     const matchesCategory =
@@ -23,10 +27,18 @@ const ExpensePanel = ({
     return matchesMonth && matchesCategory;
   });
 
+  // total money spent by month
   const totalSpent = filteredExpenses.reduce(
     (sum, expense) => sum + expense.amount,
     0,
   );
+
+  // available months
+  const availableMonths = Array.from(
+    new Set(editedProfile.expenses.map((e) => e.date.slice(0, 7))),
+  )
+    .sort()
+    .reverse();
 
   return (
     <>
@@ -44,18 +56,53 @@ const ExpensePanel = ({
         </div>
 
         <div className="flex justify-between items-center bg-gray-300 p-2 rounded-lg">
-          <select name="" id="">
-            {selectedMonth}
-          </select>
-          <select name="" id="">
-            {selectedCategory}
-          </select>
-          <span className="text-2xl text-red-600 font-bold">
+          <div className="flex justify-center items-center gap-4">
+            <select
+              name="availableMonths"
+              id="availableMonths"
+              value={selectedMonth}
+              onChange={(e) => setSelectedMonth(e.target.value)}
+              className="select"
+            >
+              {availableMonths.map((month) => (
+                <option key={month} value={month} className="px-2">
+                  {new Date(month + "-01").toLocaleDateString("pl-PL", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </option>
+              ))}
+            </select>
+            <select
+              name="categories"
+              id="categories"
+              value={(selectedCategory as ExpenseCategory) ?? "all"}
+              onChange={(e) =>
+                setSelectedCategory(
+                  e.target.value === "all"
+                    ? null
+                    : (e.target.value as ExpenseCategory),
+                )
+              }
+              className="select"
+            >
+              <option value="all" className="px-2">
+                Wszystkie
+              </option>
+              {EXPENSE_CATEGORIES.map((category) => (
+                <option key={category} value={category} className="px-2">
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <span className="text-2xl text-red-600 font-bold font-mono">
             -{totalSpent.toFixed(2)}zł
           </span>
         </div>
 
-        <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex flex-col justify-center items-center gap-2 mt-4">
           {filteredExpenses.length === 0 ? (
             <span className="text-center">Brak wydatków</span>
           ) : (
