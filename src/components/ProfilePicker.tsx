dbaa2ee18bc1dfe3ba9f1bd784cd05d28b03090e
Konrad Ciesielski <kondykox@gmail.com>
2026-08-26 08:23:54 +0200

    style: profile picker page

--- src/components/ProfilePicker.tsx
+++ src/components/ProfilePicker.tsx
@@ -19,11 +19,11 @@ const ProfilePicker = ({
   }, []);
 
   return (
-    <div className="text-center flex justify-center items-center flex-col">
+    <div className="text-center flex justify-center items-center flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:static md:translate-0 md:p-6">
       <h1 className="text-5xl font-bold text-orange-900">Siema mordo!</h1>
       <div className="mt-5">
         <h3 className="italic text-xl pb-2">Wybierz swój profil</h3>
-        <div className="border-t-2 py-2 flex justify-center items-center gap-2">
+        <div className="border-t-2 py-2 flex justify-center items-stretch gap-2">
           {profiles.map((profile) => (
             <ProfileIcon
               key={profile.id}
