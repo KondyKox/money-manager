da6a07c4f0aa977aded36823a54d48171bcde951
Konrad Ciesielski <108412658+KondyKox@users.noreply.github.com>
2026-09-01 10:17:11 +0200

    feat: added new income category

--- src/constants/incomeCategories.ts
+++ src/constants/incomeCategories.ts
@@ -1,3 +1,3 @@
-export const INCOME_CATEGORIES = ["Praca", "Prezenty", "Inne"] as const;
+export const INCOME_CATEGORIES = ["Praca", "Prezenty", "Oszczędności", "Inne"] as const;
 
 export type IncomeCategory = (typeof INCOME_CATEGORIES)[number];
